a7cd8de3e0de80d5bdbc4cc156c825867f901cc5
Armando <armando.ortiz.jr.19@outlook.com>
2023-03-09 17:48:52 -0600

    Populate and format products section

--- shopping-cart/src/components/App.js
+++ shopping-cart/src/components/App.js
@@ -4,7 +4,7 @@ import lemongrab from '../images/lemongrab.png'
 const App = () => {
   return (
     <div id="app-container">
-      <div className='imgContainer'>
+      <div className='img-container'>
         <img src={lemongrab} alt="The earl of Lemongrab, a yellow lemon humanoid"/>
       </div>
       <h3>Hmmm don't buy my stuff</h3>
--- shopping-cart/src/components/Products.js
+++ shopping-cart/src/components/Products.js
@@ -1,7 +1,102 @@
 
+import seedWad from '../images/seedwad.png'
+import soundSword from '../images/sound-sword.png';
+import lemonCamel from '../images/lemoncamel.png';
+import catchersMitt from '../images/catchersmitt.png';
+import sleepingCap from '../images/sleeping-cap.png';
+import harpSmasher from '../images/harp-smasher.png';
 const Products = () => {
     return(
-      <h1>This is the</h1>
+      <div id='products-container'>
+          <div id="grid-container">
+              <div className="grid-cell">
+                  <div className="image-container small">
+                      <img src={seedWad} alt="Seedwad, a sentient pile of candy"/>
+                  </div>
+                  <div className="grid-cell-bottom">
+                      <div className="description-container"><h2>Seedwad</h2></div>
+                      <div className="price-container">
+                          <h3>$59.99</h3>
+                          <button>Add to cart</button>
+                      </div>
+                      
+                  </div>
+              </div>
+              <div className="grid-cell">
+                  <div className="image-container small">
+                      <img src={soundSword} alt="Lemongrab's weapon, the sound sword"/>
+                  </div>
+                  <div className="grid-cell-bottom">
+                      <div className="description-container">
+                          <h2>Sound Sword</h2>
+                      </div>
+                      <div className="price-container">
+                          <h3>$1,200.32</h3>
+                          <button>Add to cart</button>
+                      </div>
+                     
+                  </div>
+              </div>
+              <div className="grid-cell">
+                  <div className="image-container small">
+                      <img src={lemonCamel} alt="LemonCamel, Lemongrab's camel made out of lemon candy"/>
+                  </div>
+                  <div className="grid-cell-bottom">
+                      <div className="description-container">
+                          <h2>Lemon Camel</h2>
+                      </div>
+                      <div className="price-container">
+                          <h3>$500.99</h3>
+                          <button>Add to cart</button>
+                      </div>
+                  </div>
+              </div>
+              <div className="grid-cell">
+                  <div className="image-container small">
+                      <img src={catchersMitt} alt="A baseball catcher's mitt"/>
+                  </div>
+                  <div className="grid-cell-bottom">
+                      <div className="description-container">
+                          <h2>Catcher's Mitt</h2>
+                      </div>
+                      <div className="price-container">
+                          <h3>$55.32</h3>
+                          <button>Add to cart</button>
+                      </div>
+                 
+                  </div>
+              </div>
+              <div className="grid-cell">
+                  <div className="image-container small">
+                      <img src={sleepingCap} alt="A blue sleeping cap"/>
+                  </div>
+                  <div className="grid-cell-bottom">
+                      <div className="description-container">
+                          <h2>Lemonsweet's Cap</h2>
+                      </div>
+                      <div className="price-container">
+                          <h3>$100,237</h3>
+                          <button>Add to cart</button>
+                      </div>
+                 
+                  </div>
+              </div>
+              <div className="grid-cell">
+                  <div className="image-container small">
+                      <img src={harpSmasher} alt="Mallet used to smash harps"/>
+                  </div>
+                  <div className="grid-cell-bottom">
+                      <div className="description-container">
+                          <h2>Harp Smasher</h2>
+                      </div>
+                      <div className="price-container">
+                          <h3>$23.18</h3>
+                          <button>Add to cart</button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
     );
 };
 
