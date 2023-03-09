c95e0cdcc6abfcd8a50a84fd510da5ea7d439bff
Armando <armando.ortiz.jr.19@outlook.com>
2023-03-09 12:57:19 -0600

    Add test to see if Home link works

--- shopping-cart/src/RouteSwitch.test.js
+++ shopping-cart/src/RouteSwitch.test.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
-import App from './components/App';
 import '@testing-library/jest-dom'
-import {render, screen, wait } from "@testing-library/react";
+import {render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import RouteSwitch from "./RouteSwitch";
 import { act } from 'react-dom/test-utils';
-import { createMemoryHistory } from 'history';
-import { isContentEditable } from '@testing-library/user-event/dist/utils';
 
 jest.mock("./components/App", () => () => (
     <h1>This is the app component</h1>
@@ -29,12 +25,23 @@ describe("RouteSwitch component", () => {
     });
 
     it("renders Products when products is clicked by user", async () => {  
-        const history = createMemoryHistory();     
-        render(<RouteSwitch history={history}/>);
+        render(<RouteSwitch/>);
         const productLink = screen.getByRole("link", {name : /Products/i});
 
         await act(async () => userEvent.click(productLink));
 
         expect(screen.getByText(/This is the products component/)).toBeInTheDocument();
+    });
+
+    it("renders Home when Home is clicked by user", async() => {
+        render(<RouteSwitch/>);
+        const productLink = screen.getByRole("link", {name : /Products/i});
+        const homeLink = screen.getByRole("link", {name: /Home/i});
+
+        await act(async () => userEvent.click(productLink));
+
+        await act(async () => userEvent.click(homeLink));
+
+        expect(screen.getByText(/This is the app component/)).toBeInTheDocument();
     });
 });
